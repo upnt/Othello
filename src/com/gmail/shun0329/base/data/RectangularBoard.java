04fdda864c270fe64346f8959b8fce081b0c89c1
upnt <base.shun0329@gmail.com>
2021-03-15 20:05:41 +0900

    add RectangularBoard class and main class(Othello class)

---
+++ src/com/gmail/shun0329/base/data/RectangularBoard.java
@@ -0,0 +1,36 @@
+package com.gmail.shun0329.base.data;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public final class RectangularBoard<T> implements Board<T> {
+	private List<Line<T>> board = new ArrayList<>();
+	public RectangularBoard(int width, int height) {
+		Line<T> line = new Line<>();
+		for(int i = 0; i < width; i++)
+			line.add(null);
+
+		for(int i = 0; i < height; i++)
+			try {
+				board.add(line.clone());
+			} catch (CloneNotSupportedException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	@Override
+	public void add(int width, int height, T value) {
+		board.get(height).set(width, value);
+	}
+	
+	@Override
+	public T get(int width, int height) {
+		return board.get(height).get(width);
+	}
+	
+	@Override
+	public Iterator<Line<T>> iterator() {
+		return board.iterator();
+	}
+}
---
+++ src/com/gmail/shun0329/base/game/Othello.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.gmail.shun0329.base.game;
+
+import com.gmail.shun0329.base.data.Board;
+import com.gmail.shun0329.base.data.Line;
+import com.gmail.shun0329.base.data.RectangularBoard;
+
+/**
+ * @author shun
+ *
+ */
+public final class Othello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Board<Integer> board = new RectangularBoard<>(4, 4);
+		board.add(1, 2, 12);
+		
+		for(Line<Integer> line : board) {
+			for(Integer i : line) {
+				if(i == null)
+					System.out.print("  ");
+				else
+					System.out.print(i);
+				System.out.print(", ");
+			}
+			System.out.println();
+		}
+	}
+
+}
